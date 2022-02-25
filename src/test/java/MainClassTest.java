33339bbc8fb59d36a5cbfbad6fb8a1c99d1931cd
3566844 <3566844@gmail.com>
2022-02-25 14:12:00 +0300

    my first Test

--- src/test/java/MainClassTest.java
+++ src/test/java/MainClassTest.java
@@ -1,8 +1,18 @@
+import com.sun.source.tree.AssertTree;
+import org.testng.Assert;
+
 import org.testng.annotations.Test;
 
+import static org.testng.AssertJUnit.assertEquals;
+import static org.testng.AssertJUnit.assertTrue;
+
 public class MainClassTest {
+
     @Test
     public void FirstTest(){
-        System.out.printf("do samesing");
+        Zebra zebra = new Zebra();
+        String food = zebra.Food();
+        assertEquals(food,"трава.");
+
     }
 }
